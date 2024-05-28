7ecb73cf500e27589029c6344b0644ef1ba6dc25
Fernichan <fsegura.r06@gmail.com>
2024-05-27 21:39:32 -0400

    Modificacion index.js y mejoracion de estilos CSS

--- src/analyzer.js
+++ src/analyzer.js
@@ -1,28 +1,56 @@
 const analyzer = {  
-  getWordCount: (text) => {
-    const analyzer = {
-      getWordCount: (text) => {
-        const words = text.trim().split(/\s+/);
-        return text.trim() ? words.length : 0;
-      },
-    };
-     //TODO: esta función debe retornar el recuento de palabras que se encuentran en el parámetro `text` de tipo `string`.
-  },
   getCharacterCount: (text) => {
+
+    return "Caracteres: " + text.length;
     //TODO: esta función debe retornar el recuento de caracteres que se encuentran en el parámetro `text` de tipo `string`.
   },
   getCharacterCountExcludingSpaces: (text) => {
+
+    return "Caracteres sin espacio: " + text.replace( /\s/g, '').length;
     //TODO: esta función debe retornar el recuento de caracteres excluyendo espacios y signos de puntuación que se encuentran en el parámetro `text` de tipo `string`.
   },
-  getAverageWordLength: (text) => {    
-    //TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro `text` de tipo `string`.
+  getWordCount: (text) => {
+    
+    return "Palabras: " + text.split(' ').length;
+    //TODO: esta función debe retornar el recuento de palabras que se encuentran en el parámetro `text` de tipo `string`.
   },
   getNumberCount: (text) => {
+    const matches = text.match(/\d+/g)
+    return "Números: " + (matches ? matches.length : 0);
     //TODO: esta función debe retornar cúantos números se encuentran en el parámetro `text` de tipo `string`.
   },
   getNumberSum: (text) => {
+    const matches = text.match(/\d+/g);
+    if (!matches) {
+      return "Suma de números: " + 0;
+    }
+    else
+    {
+      let sum = 0;
+      for (let i = 0; i < matches.length; i++) {
+        sum += parseFloat(matches[i]);
+      }
+      
+      return "Suma de números: " + sum;
+    }
     //TODO: esta función debe retornar la suma de todos los números que se encuentran en el parámetro `text` de tipo `string`.
   },
+  getAverageWordLength: (text) => {    
+    const words = text.trim().split(/\s+/);
+    if (words.length === 0 || words[0] === ""){
+      return "Promedio longitud: " + 0;
+    }
+    else
+    {
+      let totalLength = 0;
+      for (let i = 0; i < words.length; i++) {
+        totalLength += words[i].length;
+      }
+
+      return "Promedio longitud: " + totalLength / words.length;
+    }
+    //TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro `text` de tipo `string`.
+  }
 };
 
 export default analyzer;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 import analyzer from './analyzer.js';
 
-//TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
+const charCount =  document.querySelector('[data-testid="charCount"]');
+const charCountNoSpaces = document.querySelector('[data-testid="charCountNoSpaces"]');
+const wordCount = document.querySelector('[data-testid="wordCount"]');
+const numberCount = document.querySelector('[data-testid="numberCount"]');
+const numberSum = document.querySelector('[data-testid="numberSum"]');
+const avgWordLength = document.querySelector('[data-testid="avgWordLength"]');
+
+/* escuchar eventos caja de texto*/
+const text = document.querySelector('textarea[name="user-input"]');
+text.addEventListener("input", updateMetrics); /* rescatar valor ingresado por el usuario en caja de texto */
+
+function updateMetrics() {
+    charCount.textContent = analyzer.getCharacterCount(text.value);
+    charCountNoSpaces.textContent = analyzer.getCharacterCountExcludingSpaces(text.value);
+    wordCount.textContent = analyzer.getWordCount(text.value);
+    numberCount.textContent = analyzer.getNumberCount(text.value);
+    numberSum.textContent = analyzer.getNumberSum(text.value);
+    avgWordLength.textContent = analyzer.getAverageWordLength(text.value);
+  }
+
+/* escuchar evento boton reset*/
+const botonReset = document.getElementById("resetButton");
+botonReset.addEventListener("click", limpiar); /* identificamos y traemos el elemento boton */
+  
+function limpiar(){
+    text.value = "";
+    updateMetrics();
+}
+
+
+//TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer` 
